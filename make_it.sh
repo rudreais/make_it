6182a100486e9daddd620151b9ef81975ac05961
valentin.lattay <valentin.lattay@epitech.eu>
2018-02-17 17:26:32 +0100

    creating makefile issues with tab inside makefile

--- make_it.sh
+++ make_it.sh
@@ -32,6 +32,17 @@ src=()
 # 2 = sources not found
 # 3 =
 
+## SOME USEFUL FUNCTIONS
+function get_size_arr {
+    arr=("$@")
+    local index=0
+    for i in ${arr[@]}
+    do
+	index=$(echo $index + 1 | bc)
+    done
+    echo $index
+}
+
 ## GET USER INPUT AND CHECK IT
 function user_input {
     printf "\e[1mSources\e[0m directory name\n> "
@@ -97,6 +108,7 @@ function build_lib {
     dirlib_content+=($(ls $dirlib))
     dirlib_nb=$(ls $dirlib | wc -w)
     add="no"
+    index=0
     if [ -z "$dirlib_content" ]
     then
 	printf "\nThis directory is empty. Leaving flags as they are.\n"
@@ -104,16 +116,17 @@ function build_lib {
 	printf "\nFound $bold_green%s$end_char directory/ies !\n" $dirlib_nb
 	for i in ${dirlib_content[@]}
 	do
-	    printf "Do you want to add \e[1m$i\e[0m ? (Y/n)\n> "
+	    printf "Do you want to add \e[1m$i\e[0m ? (Y/n)> "
 	    read add
 	    if [ "$add" == "yes" ] || [ "$add" == "y" ] || [ -z "$add" ]
 	    then
 		printf "Adding $bold_green$i$end_char to the libraries.\n"
-		ldflags+=("-L $dirlib/$i ")
-		ldflags+="-l $i "
+		ldflags+=("-L$dirlib/$i -l$i")
 	    else
 		printf "Skipping \e[1m$i\e[0m\n"
+		unset dirlib_content[$index]
 	    fi
+	    index=$(echo $index + 1 | bc)
 	done
     fi
 }
@@ -129,34 +142,93 @@ function add_cflags {
 ## ADDING TO MAKEFILE
 function add_makefile {
     index=1
-    printf "Adding \e[1mheader\e[0m\n\n"
-    printf "$header\n" > Makefile
+    max_index=0
+    printf "Adding \e[1mEPITECH header\e[0m\n\n"
+    printf "$header\n\n" > Makefile
 
     printf "Adding \e[1mC flags\e[0m\n"
     printf "CFLAGS = ${cflags[*]}\n\n"
-    printf "CFLAGS\t\t+=\t\t${cflags[*]}\n\n" >> Makefile
+    printf "CFLAGS+= ${cflags[*]}\n\n" >> Makefile
 
     printf "Adding \e[1mlibraries flags\e[0m\n"
     printf "LDFLAGS = ${ldflags[*]}\n\n"
-    printf "LDFLAGS\t+=\t\t${ldflags}\n\n" >> Makefile
+    max_index=$(get_size_arr ${ldflags[@]})
+    for i in ${ldflags[@]}
+    do
+	if [ "$index" -eq 1 ]
+	then
+	    printf "LDFLAGS+= $i" >> Makefile
+	else
+	    printf "$i\n" >> Makefile
+	fi
+	index=$(echo $index + 1 | bc)
+    done
+    index=1
+    printf "\n" >> Makefile
 
-    printf "Adding \e[1mlibraries direcrory\e[0m\n"
+    printf "Adding \e[1mlibraries directory\e[0m\n"
+    for i in ${dirlib_content[@]}
+    do
+	printf "DIRLIB=${dirlib[*]}/$i\n\n" >> Makefile
+	printf "DIRLIB = ${dirlib[*]}/$i\n"
+    done
+    printf "\n"
 
     printf "Adding \e[1msources directory\e[0m\n"
+    printf "DIR = ${dirsrc[*]}\n\n"
+    printf "DIR=${dirsrc[*]}\n\n" >> Makefile
 
     printf "Adding \e[1msources\e[0m\n"
+    printf "SRC = ${src[*]}\n\n"
+    max_index=$(get_size_arr ${src[@]})
+    for i in ${src[@]}
+    do
+	if [ "$index" -eq 1 ]
+	then
+	    printf "SRC=$i \\ \n" >> Makefile
+	elif [ "$index" -eq "$max_index" ]
+	then
+	    printf "\t$i \n\n" >> Makefile
+	else
+	    printf "\t$i \\ \n" >> Makefile
+	fi
+	index=$(echo $index + 1 | bc)
+    done
+    index=1
 
     printf "Adding \e[1mbinary name\e[0m\n"
+    printf "EXEC=$1\n\n" >> Makefile
+    printf "EXEC = $1\n\n"
+
+    printf "OBJ=\$(SRC:.c=.o)\n\n" >> Makefile
 
     printf "Adding \e[1m\"all\" rule\e[0m\n"
+    printf "all:\$(EXEC)\n\n"
+    printf "all:\$(EXEC)\n\n" >> Makefile
 
     printf "Adding \e[1m\"\$(EXEC)\" rule\e[0m\n"
+    printf "\$(EXEC):\$(OBJ)\n" >> Makefile
+    printf "\tmake -C \$(DIRLIB)\n" >> Makefile
+    printf "\t\$(CC) -o \$(EXEC) \$(OBJ) \$(LDFLAGS) \$(CFLAGS)\n\n" >> Makefile
+    printf "\$(EXEC):\$(OBJ)\n"
+    printf "make -C \$(DIRLIB)\n"
+    printf "\$(CC) -o \$(EXEC) \$(OBJ) \$(LDFLAGS) \$(CFLAGS)\n\n"
 
     printf "Adding \e[1m\"clean\" rule\e[0m\n"
+    printf "clean:\n" >> Makefile
+    printf "\tmake -C \$(DIRLIB) clean\n" >> Makefile
+    printf "\trm -f \$(OBJ)\n\n" >> Makefile
+    printf "clean:\n"
+    printf "make -C \$(DIRLIB) clean\n"
+    printf "rm -f \$(OBJ)\n\n"
 
     printf "Adding \e[1m\"fclean\" rule\e[0m\n"
+    printf "fclean:clean\n" >> Makefile
+    printf "\tmake -C \$(DIRLIB) fclean\n" >> Makefile
+    printf "\trm -f \$(EXEC)\n\n" >> Makefile
 
-    printf "Adding \e[1m\"clean\" rule\e[0m\n"
+    printf "Adding \e[1m\"re\" rule\e[0m\n"
+    printf "re:fclean all\n\n" >> Makefile
     exit 0
 }
 
@@ -164,9 +236,12 @@ function main {
     user_input
     check_user_input
     add_cflags
+    printf "\n"
     build_src
+    printf "\n"
     build_lib
-    add_makefile
+    printf "\n"
+    add_makefile $1
 }
 
-main
+main $1
